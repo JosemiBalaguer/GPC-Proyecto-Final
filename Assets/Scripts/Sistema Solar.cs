ffb331c34f77f135db85ad2fb78f5b5cde9c238d
JosemiBalaguer <josemibalaguer@gmail.com>
2020-05-21 18:02:19 +0200

    Initial Folder Commit

---
+++ Assets/Scripts/Sistema Solar.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SistemaSolar : MonoBehaviour
+{
+    public bool orbit = false;
+    public bool translate = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (orbit)
+            transform.RotateAround(transform.parent.position, transform.up, 30 * Time.fixedDeltaTime);
+        if (translate)
+            transform.Translate(transform.up * Time.fixedDeltaTime * 3);
+    }
+}
